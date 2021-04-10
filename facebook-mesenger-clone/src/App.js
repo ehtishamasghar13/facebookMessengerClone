6c84fbe803ae4af313b26a41a99dd7748b310c2f
ehtishamasghar13 <ehtishamasghar13@gmail.com>
2021-04-10 14:30:25 +0500

    store message in array

--- facebook-mesenger-clone/src/App.js
+++ facebook-mesenger-clone/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [input, setInput]=useState('');
+  const [messages, setMessages] =useState(['Hellow','Hi','whatsup']);
+  console.log(input);
+  console.log(messages);
+  const sendMessage=(event) => {
+    //all the logic to send a message goes 
+    setMessages([...messages, input]);
+    setInput('');
+  }
   return (
     <div className="App">
       <h1>Hellow programmers</h1>
+      <input value={input} onChange={event => setInput(event.target.value)} />
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
