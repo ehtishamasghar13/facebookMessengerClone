27fe10a885b2c0824411795667876e5661ceefe1
ehtishasmasghar13 <ehtishamasghar13@gmai.com>
2021-04-10 22:43:01 -0700

    inserting and saving text done

--- facebook-mesenger-clone/src/App.js
+++ facebook-mesenger-clone/src/App.js
@@ -1,21 +1,42 @@
-import './App.css';
 import React, {useState} from 'react';
+import  Button from '@material-ui/core/Button';
+import FormControl  from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel'
+import Message from './Message'
+import './App.css';
+
 
 function App() {
   const [input, setInput]=useState('');
   const [messages, setMessages] =useState(['Hellow','Hi','whatsup']);
-  console.log(input);
-  console.log(messages);
+  //console.log(input);
+  //console.log(messages);
   const sendMessage=(event) => {
-    //all the logic to send a message goes 
+    event.preventDefault();
     setMessages([...messages, input]);
     setInput('');
   }
   return (
     <div className="App">
       <h1>Hellow programmers</h1>
-      <input value={input} onChange={event => setInput(event.target.value)} />
-      <button onClick={sendMessage}>Send Message</button>
+      <form>
+      <FormControl>
+        <InputLabel >Enter a message...</InputLabel>
+        <Input value={input} onChange={event => setInput(event.target.value)} />
+        {<Button variant="contained" color="primary" disabled={!input} onClick={sendMessage} type="submit">Send Message</Button> }
+      </FormControl>
+        {/* <button variant="outlined" onClick={sendMessage} type="submit">Send Message</button> */}
+        
+
+      </form>
+
+      {
+        messages.map(message => (
+          <Message text={message}/>
+        ))
+      }
+      
     </div>
   );
 }
